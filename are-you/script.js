d10a5816598c71e9967da4f56b7bbffaed056c51
prtvi <prithvippv25@gmail.com>
2023-01-25 15:20:38 +0530

    are-you: add custom question rather than just subject

--- are-you/script.js
+++ are-you/script.js
@@ -1,13 +1,11 @@
 'use strict';
 
 const form = document.querySelector('.form');
-const q = document.querySelector('div.q span');
+const question = document.querySelector('div.q span');
 const optns = document.querySelectorAll('div.optns input');
 const btn = document.querySelector('.btn');
 const btnContainer = document.querySelector('.submit');
 const img = document.querySelector('img');
-
-// modal
 const modal = document.querySelector('.modal');
 const modalContent = document.querySelector('.modal-content');
 const modalClose = document.querySelector('span.close');
@@ -17,21 +15,34 @@ const { right: btnR, left: btnL } = btn.getBoundingClientRect();
 const { left, right, top: up, bottom } = btnContainer.getBoundingClientRect();
 const contCenter = (right - left) / 2 + left;
 
-const setbtnXCoord = x => (btn.style.left = `${x}px`);
+// default constants
+const defQuestion = 'Are you stupid';
+const defAnswer = 'yes';
+const defMessage = 'Hehe knew it! 😂';
 
 const getUrlParams = function () {
+	// get the url params or the default values to the same
 	const url = window.location.href;
 	const params = url.slice(url.indexOf('?'));
 	const searchParams = new URLSearchParams(params);
 
-	const q = searchParams.get('q') || 'stupid';
-	const a = searchParams.get('a') || 'yes';
-	const m = searchParams.get('m') || 'Thanks for submitting!';
+	const q = searchParams.get('q') || defQuestion;
+	const a = searchParams.get('a') || defAnswer;
+	const m = searchParams.get('m') || defMessage;
 
 	return [q, a, m];
 };
 
+// set the question to the input
+const setQuestion = ques => (question.textContent = ques);
+
+// set the submit btn X coordinate
+const setbtnXCoord = x => (btn.style.left = `${x}px`);
+
+// event listeners
+
 const optionsEL = function () {
+	// on options click, enable btn and check if answer is right and enable animation accordingly
 	btn.disabled = false;
 	clickedAns = this.value;
 
@@ -39,13 +50,15 @@ const optionsEL = function () {
 	else enableWrongAnsAnimation = false;
 };
 
-const btnEL = function () {
+const btnSubmitEL = function () {
+	// on submit btn click, check if answer is as expected, if yes  then display the message and the image
 	if (clickedAns !== ans) return;
 	form.innerHTML = `<h1>${message}</h1>`;
 	img.style.display = 'block';
 };
 
 const mouseMoveEL = function (e) {
+	// on-mouse-move, check if animation is enabled, if yes then check the bounds of the mouse and move the submit btn accordingly
 	if (!enableWrongAnsAnimation) return;
 
 	const cx = e.clientX;
@@ -60,12 +73,13 @@ const mouseMoveEL = function (e) {
 };
 
 const modalOpenEL = function () {
+	// on click, open modal, select all elements in modal form, generate url and let use copy the same
 	modal.style.display = 'block';
 
 	const copyUrlBtn = modal.querySelector('.btn-modal');
 
-	const q = modal.querySelector('#subjectModal');
-	const m = modal.querySelector('#message');
+	const q = modal.querySelector('#q');
+	const m = modal.querySelector('#m');
 	const aYes = modal.querySelector('#yesModal');
 	const aNo = modal.querySelector('#noModal');
 
@@ -79,8 +93,8 @@ const modalOpenEL = function () {
 
 	[q, m, aYes, aNo].forEach(i => {
 		i.addEventListener('change', () => {
-			const qVal = q.value || 'stupid';
-			const mVal = m.value || 'Thanks for submitting!';
+			const qVal = q.value || defQuestion;
+			const mVal = m.value || defMessage;
 
 			url.value = `?q=${qVal}&a=${checkOption()}&m=${mVal}`;
 		});
@@ -100,7 +114,7 @@ const modalOpenEL = function () {
 
 // add event listeners
 optns.forEach(o => o.addEventListener('click', optionsEL));
-btn.addEventListener('click', btnEL);
+btn.addEventListener('click', btnSubmitEL);
 window.addEventListener('mousemove', mouseMoveEL);
 
 // modal close and open
@@ -114,10 +128,10 @@ window.addEventListener('click', function (e) {
 let clickedAns;
 let enableWrongAnsAnimation = false;
 btn.disabled = true;
-const [subject, ans, message] = getUrlParams();
-q.textContent = subject;
+const [ques, ans, message] = getUrlParams();
+setQuestion(ques);
 
 // validate device
 const userAgent = navigator.userAgent.toLowerCase();
 if (userAgent.includes('android') || userAgent.includes('mobile'))
-	form.innerHTML = `<h1>This page is best viewed on desktop/laptops</h1>`;
+	form.innerHTML = `<h2>This page is best viewed on desktop/laptops</h2>`;
